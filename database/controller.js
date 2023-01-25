a829c1cf90cf9a34896083446522f4df2f1d34fb
Steves1996 <steveskamdem6@gmail.com>
2023-01-25 22:53:47 +0100

    make crud api

--- database/controller.js
+++ database/controller.js
@@ -1,6 +1,13 @@
 /**Controller */
+
 import Users from "../model/user"
 
+/**
+ * Get all users
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
 export async function getUsers(req, res) {
     try {
         const users = await Users.find({})
@@ -9,4 +16,79 @@ export async function getUsers(req, res) {
     } catch (error) {
         res.status(404).json({ error: "Error while fetching data" })
     }
+}
+
+/**
+ * Get one user
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function getUser(req, res) {
+    try {
+        const { userId } = req.query;
+
+        if (userId) {
+            const user = await Users.findById(userId);
+            res.status(200).json(user)
+        }
+        res.status(404).json({ error: "User not selected" })
+    } catch (error) {
+        res.status(404).json({ error: "Cannot get the user" })
+    }
+}
+
+/**
+ * Create user
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+export async function postUser(req, res) {
+    try {
+        const formData = req.body;
+        if (!formData) return res.status(404).json({ error: "Form data not Provided" })
+        Users.create(formData, function (err, data) {
+            return res.status(200).json(data)
+        })
+    } catch (error) {
+        return res.status(404).json({ error })
+    }
+}
+
+/**
+ * Update user
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function putUser(req, res) {
+    try {
+        const { userId } = req.query;
+        const formData = req.body;
+
+        if (userId && formData) {
+            await Users.findByIdAndUpdate(userId, formData);
+            res.status(200).json(formData)
+        }
+        res.status(404).json({ error: "User not selected" })
+    } catch (error) {
+        res.status(404).json({ error: "Error while updating the data" })
+    }
+}
+
+
+export async function deleteUser(req, res) {
+
+    try {
+        const { userId } = req.query;
+
+        if (userId) {
+            const user = await Users.findByIdAndDelete(userId)
+            return res.status(200).json({ deleted: userId })
+        }
+
+        res.status(404).json({ error: "User not selected" })
+    } catch (error) {
+        res.status(404).json({ error: "Error while deleting thez user" })
+    }
+
 }
--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -1,18 +1,22 @@
 import connectMongo from '../../../database/conn';
-import { getUsers, postUser, putUser, deleteUser } from '../../../database/controller';
+import { getUser, putUser, deleteUser } from '../../../database/controller';
 
 
 export default async function handler(req, res) {
+
     connectMongo().catch(() => res.status(405).json({ error: "Error in the connection" }))
 
     const { method } = req
 
     switch (method) {
-        case 'GET':
-            getUsers(req, res);
+        case "GET":
+            getUser(req, res);
             break;
-        case 'POST':
-            postUser(req,res);
+        case 'PUT':
+            putUser(req, res);
+            break;
+        case 'DELETE':
+            deleteUser(req, res);
             break;
         default:
             res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
